e06bf95e770b03406265876e741acb47b51caafb
Raul Lopez <81383203+snayderoz@users.noreply.github.com>
2023-09-24 13:39:53 -0500

    Add files via upload

---
+++ tf_arquitectura_web/src/test/java/pe/edu/upc/tf_arquitectura_web/TfArquitecturaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tf_arquitectura_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfArquitecturaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
